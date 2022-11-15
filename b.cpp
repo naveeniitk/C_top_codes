e172ae107548bf59bf444db4cf941e4980955473
naveeniitk <naveenkumarmathurna@gmail.com>
2022-11-15 18:10:08 +0530

    made a, b .cpp

--- b.cpp
+++ b.cpp
@@ -10,13 +10,8 @@ using namespace std;
 long long mod = 1e9+7, dom = 998244353, inf = 2e18+5;
 
 void solve(int tc){
-    int n;cin >> n;    
-    vc<int> a(n), cs(n+1, 0), ze(n+1, 0);
-    for(int i=0; i<n; i++){
-        cin >> a[i];
-        cs[i+1] = cs[i] + a[i];
-        ze[i+1] = ze[i] + ((a[i]==0)?1: 0);
-    }
+    int n; cin >> n;
+    vc<int> a(n); for(int &i: a)cin >> i;
     
 }
 
